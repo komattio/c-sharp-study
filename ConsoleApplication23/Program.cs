9c2b6952985890f014d3310c79df55a68a006345
komattio <y.komatsu9@gmail.com>
2016-05-02 23:53:05 +0900

    projectの追加

---
+++ ConsoleApplication23/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication23
+{
+    class Complex
+    {
+        private double re;
+        private double im;
+
+        public double Re() { return this.re; }
+        public void Re(double x) { this.re = x; }
+
+        public double Im() { return this.im; }
+        public void Im(double y) { this.im = y; }
+
+        public double Abs() { return Math.Sqrt(re * re + im * im); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex x = new Complex();
+            x.Re(5);
+            x.Im(1);
+
+            Complex y = new Complex();
+            y.Re(-2);
+            y.Im(3);
+
+            Complex z = new Complex();
+            z.Re(x.Re() + y.Re());
+            z.Im(x.Im() + y.Im());
+
+            Console.WriteLine("|{0} + {1}i| = {2}", z.Re(), z.Im(), z.Abs());
+        }
+    }
+}
---
+++ ConsoleApplication24/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    //class Complex
+    //{
+    //    private double re;
+    //    private double im;
+
+    //    // 実部の取得・変更用のプロパティ
+    //    public double Re
+    //    {
+    //        set { this.re = value; }
+    //        get { return this.re; }
+    //    }
+
+    //    // 虚部の取得・変更用のプロパティ
+    //    public double Im
+    //    {
+    //        set { this.im = value; }
+    //        get { return this.im; }
+    //    }
+
+    //    // 絶対値の取得用のプロパティ
+    //    public double Abs
+    //    {
+    //        get { return Math.Sqrt(re * re + im * im); }
+    //    }
+
+    //}
+
+    class Complex
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+        public double Abs
+        {
+            get { return Math.Sqrt(Re * Re + Im * Im); }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex();
+            c.Re = 4;
+            c.Im = 3;
+            Console.Write("|{0} + ", c.Re);
+            Console.Write("{0}i| = ", c.Im);
+            Console.Write(" {0}\n", c.Abs);
+        }
+    }
+}
