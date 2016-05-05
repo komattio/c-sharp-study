8b8417849e4d261cab6fc72843443a84d0430202
komattio <y.komatsu9@gmail.com>
2016-05-05 23:53:26 +0900

    delegate demo

---
+++ ConsoleApplication31/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication31
+{
+    delegate void SomeDelegate(int a);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeDelegate a = new SomeDelegate(A);
+
+            a(256);
+        }
+
+        static void A(int n)
+        {
+            Console.WriteLine("A({0})", n);
+        }
+    }
+}
